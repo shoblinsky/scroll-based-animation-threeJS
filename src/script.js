f66572b1223c5fcbc3e92399cb98ed3325791328
shoblinsky <123806946+shoblinsky@users.noreply.github.com>
2025-05-12 20:19:39 +0500

    add resize func, replace animation by tjs.Clock()

--- src/script.js
+++ src/script.js
@@ -21,6 +21,21 @@ const sizes = {
     height: window.innerHeight
 }
 
+window.addEventListener('resize', () => {
+    // Update sizes
+    sizes.width = window.innerWidth
+    sizes.height = window.innerHeight
+
+    // Update camera
+    camera.aspect = sizes.width / sizes.height
+    camera.updateProjectionMatrix()
+
+    // Update renderer
+    renderer.setSize(sizes.width, sizes.height)
+    renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+})
+
+
 // Camera
 const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height)
 camera.position.z = 3
@@ -34,14 +49,24 @@ scene.add(camera)
 const renderer = new THREE.WebGLRenderer({
     canvas: canvas
 })
+
 renderer.setSize(sizes.width, sizes.height)
+renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+
 
 // Controls
 const controls = new OrbitControls(camera, canvas)
 controls.enableDamping = true
 
 
+const clock = new THREE.Clock()
+
 const tick = () => {
+    const elapsedTime = clock.getElapsedTime()
+
+    // Update controls
+    controls.update()
+
     // Render
     renderer.render(scene, camera)
 
