74e65baa24d0e340058236377cac081df3b92da0
shoblinsky <123806946+shoblinsky@users.noreply.github.com>
2025-05-12 20:11:24 +0500

    test scene with cube and orbitcontrols

--- src/script.js
+++ src/script.js
@@ -1,17 +1,19 @@
 import * as THREE from 'three'
+import { OrbitControls } from 'three/examples/jsm/Addons.js'
 
-console.log(THREE)
-
+// Canvas
+const canvas = document.querySelector('canvas.webgl')
 
 // Scene
 const scene = new THREE.Scene()
+
 
 // Object
 const geometry = new THREE.BoxGeometry(1, 1, 1)
 const material = new THREE.MeshBasicMaterial({ color: 0xff0000 })
 const mesh = new THREE.Mesh(geometry, material)
-
 scene.add(mesh)
+
 
 // Sizes
 const sizes = {
@@ -22,11 +24,11 @@ const sizes = {
 // Camera
 const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height)
 camera.position.z = 3
+camera.lookAt(mesh.position)
 scene.add(camera)
 
 
-// Canvas
-const canvas = document.querySelector('canvas.webgl')
+
 
 // Renderer
 const renderer = new THREE.WebGLRenderer({
@@ -34,4 +36,17 @@ const renderer = new THREE.WebGLRenderer({
 })
 renderer.setSize(sizes.width, sizes.height)
 
-renderer.render(scene, camera)
+// Controls
+const controls = new OrbitControls(camera, canvas)
+controls.enableDamping = true
+
+
+const tick = () => {
+    // Render
+    renderer.render(scene, camera)
+
+    // Call tick again on the next frame
+    window.requestAnimationFrame(tick)
+}
+
+tick()
