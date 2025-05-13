c1efb1fcfa6687cc1ce5a7533f9a337a974b4d86
shoblinsky <123806946+shoblinsky@users.noreply.github.com>
2025-05-13 17:25:12 +0500

    add folders, prepare for scroll-adding

--- src/script.js
+++ src/script.js
@@ -116,13 +116,16 @@ const generateGalaxy = () => {
     scene.add(points)
 }
 
-gui.add(parameters, 'count').min(100).max(1000000).step(100).onFinishChange(generateGalaxy)
-gui.add(parameters, 'radius').min(0.01).max(20).step(0.01).onFinishChange(generateGalaxy)
-gui.add(parameters, 'branches').min(2).max(20).step(1).onFinishChange(generateGalaxy)
-gui.add(parameters, 'randomness').min(0).max(2).step(0.001).onFinishChange(generateGalaxy)
-gui.add(parameters, 'randomnessPower').min(1).max(10).step(0.001).onFinishChange(generateGalaxy)
-gui.addColor(parameters, 'insideColor').onFinishChange(generateGalaxy)
-gui.addColor(parameters, 'outsideColor').onFinishChange(generateGalaxy)
+
+
+const GalaxyFolder = gui.addFolder('Folder for galaxy background');
+GalaxyFolder.add(parameters, 'count').min(100).max(1000000).step(100).onFinishChange(generateGalaxy)
+GalaxyFolder.add(parameters, 'radius').min(0.01).max(20).step(0.01).onFinishChange(generateGalaxy)
+GalaxyFolder.add(parameters, 'branches').min(2).max(20).step(1).onFinishChange(generateGalaxy)
+GalaxyFolder.add(parameters, 'randomness').min(0).max(2).step(0.001).onFinishChange(generateGalaxy)
+GalaxyFolder.add(parameters, 'randomnessPower').min(1).max(10).step(0.001).onFinishChange(generateGalaxy)
+GalaxyFolder.addColor(parameters, 'insideColor').onFinishChange(generateGalaxy)
+GalaxyFolder.addColor(parameters, 'outsideColor').onFinishChange(generateGalaxy)
 
 
 // Sizes
